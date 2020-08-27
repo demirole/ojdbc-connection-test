68571cbc1e6b04f6b018dc0bdd15a465a92ea28f
Levent Demirörs <17856176+demirole@users.noreply.github.com>
2020-08-27 10:59:09 +0200

    Change app signature to allow for user statements

--- src/main/java/ch/demirole/OjdbcTester/App.java
+++ src/main/java/ch/demirole/OjdbcTester/App.java
@@ -11,12 +11,16 @@ import java.util.Properties;
 		version = "1.0")
 
 public class App implements Runnable {
-	@CommandLine.Parameters(index = "0", description = "connection URL")
-	private String connectionUrl;
+	private static final String TEST_QUERY = "select 1 from dual";
+
+	@CommandLine.Parameters(index = "0", description = "SQL test statement (default: select 1 from dual")
+	private String testStatement = TEST_QUERY;
 	@CommandLine.Option(names = {"-u", "--user"}, description = "user name for DB access")
 	private String userName = "";
 	@CommandLine.Option(names = {"-p", "--password"}, description = "password for DB access")
 	private String password = "";
+	@CommandLine.Option(names = {"-c", "--connnection-url"}, description = "connection URL")
+	private String connectionUrl = "";
 
 	public static void main(String... args) {
 		int exitCode = new CommandLine(new App()).execute(args);
@@ -24,14 +28,19 @@ public class App implements Runnable {
 	}
 
 	public void run() {
+		testStatement = sanitizeSQLStatement(testStatement);
 		Properties connectionProperties = createProperties(userName, password);
 		try {
-			new ConnectionTester().invoke(connectionUrl, connectionProperties);
+			new ConnectionTester().invoke(connectionUrl, connectionProperties, testStatement);
 		} catch (SQLException throwables) {
 			throwables.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private String sanitizeSQLStatement(String testStatement) {
+		return testStatement.replaceAll(";+$", "");
 	}
 
 	private Properties createProperties(String userName, String password) {
--- src/main/java/ch/demirole/OjdbcTester/ConnectionTester.java
+++ src/main/java/ch/demirole/OjdbcTester/ConnectionTester.java
@@ -5,16 +5,14 @@ import java.util.Properties;
 
 public class ConnectionTester {
 
-	private static final String TEST_QUERY = "select 1 from dual";
-
-	public void invoke(String connectionUrl, Properties connectionProperties) throws SQLException, ClassNotFoundException {
+	public void invoke(String connectionUrl, Properties connectionProperties, String testStatement) throws SQLException, ClassNotFoundException {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		Connection conn =
 				DriverManager.getConnection(connectionUrl, connectionProperties);
 		conn.setAutoCommit(false);
 		Statement stmt = conn.createStatement();
 		ResultSet rset =
-				stmt.executeQuery( TEST_QUERY);
+				stmt.executeQuery(testStatement);
 		while (rset.next()) {
 			System.out.println (rset.getString(1));
 		}
